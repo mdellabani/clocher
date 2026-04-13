fbee01eb92a99189e84ff6d9571d1000ae1bef1e
mde <mde@ext.activeviam.com>
2026-04-13 22:58:22 +0200

    fix(mobile): add react-native-gesture-handler for Android swipe-back

--- apps/mobile/src/app/_layout.tsx
+++ apps/mobile/src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import "react-native-gesture-handler";
 import { Stack, useSegments, useRouter } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
